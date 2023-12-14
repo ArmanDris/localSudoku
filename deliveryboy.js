0716311a82d77a90e6604553c3a6312230662493
Arman Drismir <adrismir@gmail.com>
2023-12-14 01:07:30 -0800

    Added logic for local and public server

--- deliveryboy.js
+++ deliveryboy.js
@@ -1,54 +1,67 @@
 class DeliveryBoy {
-  deliver(name, score) {
-    const url = `http://blueberrypie.myddns.me:5000/mailbox`;
+	url = 'https://blueberrypie.myddns.me:5000'
 
-    fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-store',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify({ name: name, score: score })
-    })
-      .then(response => {
-        if (response.ok) {
-          console.log('Score sent successfully');
-          return true;
-        }
-        else {
-          console.error('Failed to send score, Error:', response.status);
-        }
-      })
-      .catch(error => {
-        console.error('Error', error)
-      })
-  }
+	async deliver(name, score) {
 
-  async receive() {
-    try {
-      const url = `http://blueberrypie.myddns.me:5000/leaderboard`;
+		this.handleLocalServer();
 
-      const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-store',
-        headers: {
-          'Content-type': 'application/json'
-        }
-      });
+		fetch(this.url + '/mailbox', {
+			method: 'POST',
+			mode: 'cors',
+			cache: 'no-store',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({ name: name, score: score })
+		})
+			.then(response => {
+				if (response.ok) {
+					console.log('Score sent successfully');
+					return true;
+				}
+				else {
+					console.error('Failed to send score, Error:', response.status);
+				}
+			})
+			.catch(error => {
+				console.error('Error', error)
+			})
+	}
 
-      if (response.ok) {
-        const data = await response.json();
-        return data.leaderboard;
-      }
-      else {
-        throw new Error(response.status);
-      }
-    }
-    catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
+	async receive() {
+		this.handleLocalServer();
+		try {
+			const response = await fetch(this.url + '/leaderboard', {
+				method: 'POST',
+				mode: 'cors',
+				cache: 'no-store',
+				headers: {
+					'Content-type': 'application/json'
+				}
+			});
+
+			if (response.ok) {
+				const data = await response.json();
+				return data.leaderboard;
+			}
+			else {
+				throw new Error(response.status);
+			}
+		}
+		catch (error) {
+			console.log(error);
+			throw error;
+		}
+	}
+
+	async handleLocalServer() {
+		let local_url = 'http://192.168.1.75:5000';
+		try {
+			const response = await fetch(local_url + '/leaderboard');
+			if (response.ok)
+				this.url = local_url;
+		} catch (error) {
+			return;
+		}
+	}
 }
